e76646cc3c150647aaba0369dc35aa4a8f37c15f
rebecabergottini <117315880+rebecabergottini@users.noreply.github.com>
2023-01-19 16:02:41 +0000

    my first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,41 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState("");
+	const [listaTareas, setListaTareas] = useState([]);
+	const [noTareas, setMensaje] = useState("");
+
+	function handleTareas(e){
+		if(e.key === "Enter"){
+			e.preventDefault()
+			if(tareas === ""){
+				alert("Please add a task");
+			} else{
+				setListaTareas([...listaTareas, tareas]);
+				setMensaje("visually-hidden");
+				setTareas("");
+			}
+		}
+	}
+	
+	function eliminarTareas(id){
+		setListaTareas(listaTareas.filter((tarea,index) => index !== id));
+		if (listaTareas.length === 1){
+			setMensaje("");
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-left w-50 mx-auto mt-5">
+			<h1 className="text-center fw-light text-purple-500 fs-1 mb-2">todos</h1>
+			<ul className="list-group list-group">
+				<li className="list-group-item px-5">
+					<input className="w-100 border border-0 fs-5" type="text" name="todos" id="todos" placeholder="What needs to be done?" onChange= {(e)=>setTareas(e.target.value)} value={tareas} onKeyDown={handleTareas}/>
+				</li>
+				{listaTareas.map((tarea, id) => <li id="cruz" className="list-group-item fs-5 text-left px-5 d-flex justify-content-between" key={id}>{tarea}<button id="eliminarcruz" type="button" className={"btn-close fs-6 mt-1"} aria-label="Close" onClick={() => eliminarTareas(id)} ></button></li>)}
+				<li className="list-group-item text-left fs-6 fw-light text-black-50">{listaTareas.length} item left</li>
+			</ul>
 		</div>
 	);
 };
