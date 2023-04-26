1cf2abbc5cbc2426f62dcb7882af8114bb164fe1
rebecabergottini <117315880+rebecabergottini@users.noreply.github.com>
2023-04-26 14:34:00 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,84 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [tasks, setTasks] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  function handleTasks(event) {
+    setTasks(event.target.value);
+  }
+
+  function addTasks(event) {
+    if (event.key === "Enter" && tasks.trim() !== "") {
+      setToDoList([...toDoList, tasks]);
+      setTasks("");
+    }
+  }
+
+  function removeTasks(index) {
+    const newList = [...toDoList];
+    newList.splice(index, 1);
+    setToDoList(newList);
+  }
+
+  return (
+    <div className="text-left w-50 mx-auto mt-5">
+      <h1
+        className="text-center fw-light fs-1 mb-2"
+        style={{ color: "#FFC0CB" }}
+      >
+        todos
+      </h1>
+      <ul className="list-group list-group">
+        {toDoList.length === 0 ? (
+          <li className="list-group-item px-5">
+            <input
+              className="w-100 border border-0 fs-5"
+              onChange={handleTasks}
+              onKeyDown={addTasks}
+              value={tasks}
+              type="text"
+              id="todos"
+              placeholder="No tasks, add a task"
+            />
+          </li>
+        ) : (
+          <li className="list-group-item px-5">
+            <input
+              className="w-100 border border-0 fs-5"
+              onChange={handleTasks}
+              onKeyDown={addTasks}
+              value={tasks}
+              type="text"
+              id="todos"
+              placeholder="What needs to be done?"
+            />
+          </li>
+        )}
+        {toDoList.map((task, index) => (
+          <li
+            key={index}
+            id="cruz"
+            className="list-group-item fs-5 text-left px-5 d-flex justify-content-between"
+          >
+            {" "}
+            {task}
+            <button
+              id="eliminarcruz"
+              onClick={() => removeTasks(index)}
+              type="button"
+              className={"btn-close fs-6 mt-1"}
+              aria-label="Close"
+            ></button>
+          </li>
+        ))}
+        <li className="list-group-item text-left fs-6 fw-light text-black-50">
+          {toDoList.length} item left
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
